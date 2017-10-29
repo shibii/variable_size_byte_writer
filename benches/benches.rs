d66f84d588f278052f64cde0b787c6f74827a083
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2017-10-29 23:07:07 +0200

    added 'temp' directory creation to benches since the directory is ignored by git

--- benches/benches.rs
+++ benches/benches.rs
@@ -17,6 +17,7 @@ fn write_32_vec(bench: &mut Bencher) {
 #[bench]
 fn write_32_file(bench: &mut Bencher) {
     let mut writer = VariableSizeByteWriter::new(8192);
+    std::fs::create_dir("benches/temp");
     let mut file = std::fs::File::create("benches/temp/write_32_file.temp").unwrap();
     bench.iter(|| writer.write_32(&mut file, 0x7_F1F0, 21));
     std::fs::remove_file("benches/temp/write_32_file.temp").unwrap();
@@ -33,6 +34,7 @@ fn write_16_vec(bench: &mut Bencher) {
 #[bench]
 fn write_16_file(bench: &mut Bencher) {
     let mut writer = VariableSizeByteWriter::new(8192);
+    std::fs::create_dir("benches/temp");
     let mut file = std::fs::File::create("benches/temp/write_16_file.temp").unwrap();
     bench.iter(|| writer.write_16(&mut file, 0x1A, 9));
     std::fs::remove_file("benches/temp/write_16_file.temp").unwrap();
@@ -49,6 +51,7 @@ fn write_8_vec(bench: &mut Bencher) {
 #[bench]
 fn write_8_file(bench: &mut Bencher) {
     let mut writer = VariableSizeByteWriter::new(8192);
+    std::fs::create_dir("benches/temp");
     let mut file = std::fs::File::create("benches/temp/write_8_file.temp").unwrap();
     bench.iter(|| writer.write_8(&mut file, 0x1A, 7));
     std::fs::remove_file("benches/temp/write_8_file.temp").unwrap();
