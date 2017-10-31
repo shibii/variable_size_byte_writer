dc0dc2fea7262f34795ac20809315e4fee046a0c
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2017-11-01 01:58:32 +0200

    fixed benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -23,6 +23,7 @@ fn write_59bits_file(bench: &mut Bencher) {
 
     let bits = test::black_box(59);
     bench.iter(|| writer.write(&mut file, 0x7A25555_ABABFFFF, bits));
+
     std::fs::remove_file("benches/temp/write_64_file.temp").unwrap();
 }
 
@@ -32,7 +33,7 @@ fn write_21bits_vec(bench: &mut Bencher) {
     let mut target = std::io::Cursor::new(vec![]);
 
     let bits = test::black_box(21);
-    bench.iter(|| writer.write_32(&mut target, 0x7_F1F0, bits));
+    bench.iter(|| writer.write(&mut target, 0x7_F1F0, bits));
 }
 
 #[bench]
@@ -43,6 +44,7 @@ fn write_21bits_file(bench: &mut Bencher) {
 
     let bits = test::black_box(21);
     bench.iter(|| writer.write(&mut file, 0x7_F1F0, bits));
+
     std::fs::remove_file("benches/temp/write_32_file.temp").unwrap();
 }
 
@@ -52,15 +54,17 @@ fn write_7bits_vec(bench: &mut Bencher) {
     let mut target = std::io::Cursor::new(vec![]);
 
     let bits = test::black_box(7);
-    bench.iter(|| writer.write_8(&mut target, 0x1A, bits));
+    bench.iter(|| writer.write(&mut target, 0x1A, bits));
 }
 
 #[bench]
 fn write_7bits_file(bench: &mut Bencher) {
     let mut writer = VariableSizeByteWriter::new();
     let _res = std::fs::create_dir("benches/temp");
+
     let mut file = std::fs::File::create("benches/temp/write_8_file.temp").unwrap();
     let bits = test::black_box(7);
-    bench.iter(|| writer.write_8(&mut file, 0x1A, bits));
+    bench.iter(|| writer.write(&mut file, 0x1A, bits));
+
     std::fs::remove_file("benches/temp/write_8_file.temp").unwrap();
 }
